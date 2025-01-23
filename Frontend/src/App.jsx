4425633d2c32a58c9519dfeac5089b82c60bc203
datmedevil17 <rakshitshukla17@gmail.com>
2025-01-24 02:57:25 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router';
 // import Home from './pages/Home';
 import ConnectWallet from './pages/ConnectWallet';
 import About from './pages/About';
+import Launchpad from './pages/Launchpad';
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<ConnectWallet/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/launchpad" element={<Launchpad/>} />
 
       </Routes>
     </>
