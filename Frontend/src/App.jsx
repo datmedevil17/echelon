fb9edd54e70eb49267269a6339168d398733b3fb
datmedevil17 <rakshitshukla17@gmail.com>
2025-01-24 02:12:15 +0530

    Connect Wallet and navbar sorted

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './index.css';
 import { Route, Routes } from 'react-router';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import ConnectWallet from './pages/ConnectWallet';
 import About from './pages/About';
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<ConnectWallet/>} />
+        <Route path="/about" element={<About/>} />
+
       </Routes>
     </>
   );
